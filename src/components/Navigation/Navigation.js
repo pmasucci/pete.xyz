63863a5c3cd278795864bbb8d61fec726088a1f9
Peter Masucci <peter.a.masucci@gmail.com>
2018-12-19 18:32:20 -0500

    Initial setup

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Navigation extends Component {
+  render() {
+    return (
+      <nav className="nav">
+        <div> butts</div>
+      </nav>
+    );
+  }
+}
+
+export default Navigation;
