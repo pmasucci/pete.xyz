634027ada921684f2ac485c1ffb61628f1646e63
Peter Masucci <peter.a.masucci@gmail.com>
2019-08-28 13:19:28 -0400

    blank slate

--- src/components/Fun/Fun.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import { Page } from '../';
-
-class Fun extends Component {
-  render() {
-    return <div>Butts</div>;
-  }
-}
-export default Page(Fun);
